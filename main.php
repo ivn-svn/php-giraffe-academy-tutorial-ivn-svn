f0a079229896ad1a87f7254c7bdc0fdc204f65a1
svetlin <svetlin@svn-mac-pro.local>
2022-04-15 16:36:27 +0300

    daily php

---
+++ main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>Svetlin´s PHP</h1>
+
+    <?php 
+
+
+
+echo "The author is $name. $name started working on this by following a YouTube tutorial. For more info, contact $name";
+
+?>
+
+</body>
+
+</html>
