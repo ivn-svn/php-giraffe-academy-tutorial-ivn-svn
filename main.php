7f7154b14700b66e5cc6e5113a2f1345482d7150
svetlin <svetlin@svn-mac-pro.local>
2022-04-17 21:43:50 +0300

    php tutorial

--- main.php
+++ main.php
@@ -10,7 +10,25 @@
 
 echo "The author is $name. $name started working on this by following a YouTube tutorial. For more info, contact $name";
 
+
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
 ?>
+ 
+
 
 </body>
 
